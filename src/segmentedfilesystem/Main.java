7d07faba2c08c7c7188b384ecb6981a34fab4206
casey369 <casey369@morris.umn.edu>
2017-11-28 10:35:24 -0600

    Begins Main class of fileserver client

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -1,9 +1,32 @@
 package segmentedfilesystem;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class Main {
+	int port;
+	InetAddress address;
+	DatagramSocket socket = null;
+	DatagramPacket packet;
+	byte[] sendBuf = new byte[256];
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+    	
+    	
+    	if(args.length != 1){
+    		System.out.println("Using segmented client server");
+    		return;
+    	}
+    	DatagramSocket socket = new DatagramSocket();
+    	
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(args[0]);
+        DatagramPacket packet = new DatagramPacket(buf,buf.length,address,4445);
+        socket.send(packet);
     }
 
 }
